1f830b1205f7cf5b7e2b19d794deba8a35def7b4
CallmeJane <942296928@qq.com>
2020-07-21 18:37:25 +0800

    添加k-means（3步走）和pca（两步走）的使用；

---
+++ 7.k-means_clustering_and_principal_component_analysis/k-means_clustering.py
@@ -0,0 +1,118 @@
+#coding=utf-8
+
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+import cv2
+
+#初始化中心点
+def k_means_init_centroids(X,K):
+    m=X.shape[0]
+    randidx=np.random.permutation(m)          #将index随机打乱
+    centroids = X[randidx[0:K], :]            #取得随机打乱后的前K个X    return centroids
+    return centroids
+
+#对于每个点，找到最近的中线点作为一个聚类
+def find_closest_centroids(X, centroids):
+    idx=np.zeros(len(X),dtype=np.int)
+    for i,x in enumerate(X):
+        dist=np.sum((x-centroids)**2,axis=1)
+        dist=dist.tolist()
+        idx[i]=dist.index(min(dist))
+    return idx
+
+#对于每个聚类，找到新的中心点
+def computeMeans(X, idx, K):
+    means=np.zeros((K,X.shape[1]))
+    for i in range(K):
+        indexs=np.where(idx==i)
+        x=X[indexs]
+        if len(x)!=0:
+            means[i]=np.mean(x,dtype=np.int,axis=0)
+    return means
+
+def run_kmeans(epoches,X,centroids,K):
+    centroids_all=[centroids]
+    for i in range(epoches):
+        idx=find_closest_centroids(X,centroids)
+        means=computeMeans(X,idx,K)
+        centroids=means
+        centroids_all.append(centroids)
+    return centroids_all
+def plot_data(X,centroids,K):
+    plt.figure()
+    centroids=np.array(centroids).reshape(-1,K*X.shape[1])
+    centroid=centroids[-1].reshape(-1,X.shape[1])
+    print(centroid)
+    idx=find_closest_centroids(X,centroid)
+    markers=['x','o','+']
+    colors=['red','blue','green']
+    for i in range(3):
+        class_i=np.where(idx==i)
+        class_i_data=X[class_i]
+        plt.scatter(class_i_data[:,0],class_i_data[:,1],marker=markers[i],color=colors[i])
+        plt.plot(centroids[:,i*2],centroids[:,i*2+1],marker='X',color='black')
+    plt.title('Iteration number 10')
+    plt.show()
+
+if __name__ == '__main__':
+    #--------Part 1 Implementing K-means--------
+    #k-means算法的最终的中心点与初始值由很大关系；一般选择不同的初始值来确定最好的聚类中心
+    K = 2
+    X=np.arange(1,11).reshape(10,1)
+    centroids=np.arange(1,3).reshape(2,1)
+    idx=find_closest_centroids(X, centroids)
+    print(idx)
+    centroids_all=[centroids]
+    for i in range(20):
+        idx = find_closest_centroids(X, centroids)
+        means=computeMeans(X, idx, K)
+        #print(means)
+        centroids=means
+
+    #--------Part 2 K-means on example dataset--------
+    datas=loadmat('data/ex7data2.mat')
+    X=datas['X']               #50*2
+    K=3
+    #centroids=np.zeros((K,X.shape[1]))
+    centroids=np.random.rand(K,X.shape[1])
+    # centroids_all=run_kmeans(10,X,centroids,K)
+    # #print(centroids_all)
+    # plot_data(X,centroids_all,K)
+
+    #--------Part 3 Random initialization--------
+    centroids=k_means_init_centroids(X,K)
+    #centroids_all = run_kmeans(10, X, centroids, K)
+    # print(centroids_all)
+    #plot_data(X, centroids_all, K)
+
+    #--------Part 4 Image compression with K-means--------
+    # png=cv2.imread('data/bird_small.png')
+    # K=16
+    # png=png.reshape(-1,3)
+    # centroids = k_means_init_centroids(png, K)
+    # centroids_all = run_kmeans(10, png, centroids, K)
+    # #png_data=loadmat('data/bird_small.mat')
+    # centroid=np.array(centroids_all[-1]).reshape(-1,3)
+    # idx=find_closest_centroids(png,centroid)
+    # for i in range(K):
+    #     png[np.where(idx==i)]=centroid[i]
+    # png=png.reshape(128,128,3)
+    # cv2.imwrite('data/bird_small_comp.png',png)
+
+    #--------Part 5  Use your own image--------
+    #需要裁剪自己的图像；自己选择合适的K
+    K=3
+    img=cv2.imread('data/618629e1a5b302aca78d0de6447180a3.jpg')
+    print(img.shape)
+    img=cv2.resize(img,(128,128),interpolation=cv2.INTER_CUBIC)
+    print(img.shape)
+    img=img.reshape(-1,3)
+    centroids = k_means_init_centroids(img, K)
+    centroids_all = run_kmeans(10, img, centroids, K)
+    centroid=np.array(centroids_all[-1]).reshape(-1,3)
+    idx=find_closest_centroids(img,centroid)
+    for i in range(K):
+        img[np.where(idx==i)]=centroid[i]
+    img=img.reshape(128,128,3)
+    cv2.imwrite('data/618629e1a5b302aca78d0de6447180a3_comp.jpg',img)
---
+++ 7.k-means_clustering_and_principal_component_analysis/principal_component_analysis.py
@@ -0,0 +1,111 @@
+#coding=utf-8
+
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import cv2
+
+def plot_data(X,marker='o',color='blue'):
+    plt.scatter(X[:,0],X[:,1],marker=marker,color=color)
+    #plt.show()
+
+def feature_normalize(X):
+    x=X.copy().astype(np.float)                     #标准化时要注意数据的类型，一定是float
+    mean=np.mean(X,axis=0)
+    std=np.std(X,axis=0,ddof=1)
+    for i in range(len(mean)):
+        x[:,i]=(x[:,i]-mean[i])/std[i]
+    return mean,std,x
+def draw_line(p1, p2,plot_kwargs=""):
+    x=np.array([p1[0],p2[0]])
+    y=np.array([p1[1],p2[1]])
+    plt.plot(x,y,plot_kwargs)
+    #plt.show()
+
+def project_data(x_norm,U,K):
+    #将数据根据U,K降低到低维空间
+    u_reduce=U[:,0:K]
+    return np.dot(x_norm,u_reduce)
+
+def recover_data(pro_data,U,K):
+    #将低维空间恢复到原来的高维数据空间
+    u_reduce=U[:,0:K]
+    return np.dot(pro_data,u_reduce.T)
+
+def gray(f):
+    f_new = (f-np.min(f))/(np.max(f)-np.min(f))*255
+    return f_new
+
+def show_top_k_faces(X,k=100,width=32,height=32):
+    #width, height = 10, 10
+    lines,rows=int(np.sqrt(k)),int(np.sqrt(k))
+    img = np.zeros((rows * width, lines * height))
+    # max_val=np.max(np.abs(x))
+    for line in range(lines):
+        for row in range(rows):
+            x = X[line * rows + row].reshape(width, height, order='F')
+            img[line * height:(line + 1) * height, row * width:(row + 1) * width] = gray(x)  # /max_val
+    cv2.imwrite('data/top-100_faces.png', img)
+
+if __name__ == '__main__':
+    #------Part 1 Example Dataset------
+    datas=loadmat('data/ex7data1.mat')
+    X=datas['X']
+    m=len(X)
+    #plot_data(X)
+    #plt.show()
+
+    #------Part 2 Implementing PCA------
+    #不是所有的情况都适合PCA,具体要看数据的分布
+    mean,std,x_norm=feature_normalize(X)
+    #(1)compute the covariance matrix of the data, which is given by
+    SIGMA=np.dot(x_norm.T,x_norm)/m           #2*2
+    #(2)run SVD on it to compute the principal components.
+    U,S,V=np.linalg.svd(SIGMA)      #U是主成分；S是对角阵
+    #print(U,S)
+    #draw_line(mean,mean+std*np.dot(S[0],U[:,0].T),plot_kwargs='-k')        #画出新的坐标轴
+    #draw_line(mean,mean+std*np.dot(S[1],U[:,1].T),plot_kwargs='-k')
+    #plt.show()
+
+    #------Part 3 Dimensionality Reduction with PCA------
+    K=1
+    pro_data=project_data(x_norm,U,K)
+    #print(pro_data[0])
+    rec_data=recover_data(pro_data,U,K)
+    #print(rec_data[0])
+    plt.figure()
+    plot_data(x_norm)
+    rec_data_init=mean+std*rec_data
+    # plot_data(rec_data,marker='+',color='red')
+    # #为什么我画出的新的x,y并不是垂直
+    # for i in range(m):
+    #     draw_line(x_norm[i],rec_data[i],'--k')   #画虚线
+    # plt.show()
+
+    #------Part 4 Face Image Dataset------
+    faces_datas=loadmat('data/ex7faces.mat')
+    X=faces_datas['X']           #5000*1024(=32*32)
+    m=len(X)
+    #visual first 100 faces
+    mean,std,x_norm=feature_normalize(X)
+    #run pca
+    SIGMA=np.dot(x_norm.T,x_norm)/m
+    U,S,V=np.linalg.svd(SIGMA)
+    k=100                               #取前k个主成分
+    #show_top_k_faces(np.dot(np.dot(x_norm,U[:,0:k]),U[:,0:k].T), k=k)
+    #训练的时候可以只用前k维训练
+    show_top_k_faces(np.dot(x_norm,U[:,0:k]), k=k,width=10,height=10)
+
+    #------Part 5 PCA for visualization(只完成了2维可视化)------
+    png=cv2.imread('data/bird_small.png').reshape(-1,3,order='F')
+    m=len(png)
+    mean, std, x_norm = feature_normalize(png)
+    SIGMA=np.dot(x_norm.T,x_norm)/m
+    U,S,V=np.linalg.svd(SIGMA)
+
+    figure=plt.figure()
+    top_k=2
+    x_norm_ruduce=project_data(x_norm,U,top_k)
+    plt.scatter(x_norm_ruduce[0:100,0],x_norm_ruduce[0:100,1],color='red',marker='+')
+    plt.show()
